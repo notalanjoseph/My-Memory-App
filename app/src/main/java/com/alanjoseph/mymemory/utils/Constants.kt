0968353da05a59df5584c4127793f176bae876c5
Alan Joseph <notalanjoseph@gmail.com>
2023-06-06 19:55:27 +0530

    first commit

---
+++ app/src/main/java/com/alanjoseph/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.alanjoseph.mymemory.utils
+
+import com.alanjoseph.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_anchor,
+    R.drawable.ic_camera,
+    R.drawable.ic_car,
+    R.drawable.ic_coffee,
+    R.drawable.ic_dollar,
+    R.drawable.ic_football,
+    R.drawable.ic_gamepad,
+    R.drawable.ic_heart,
+    R.drawable.ic_iphone,
+    R.drawable.ic_moon,
+    R.drawable.ic_star,
+    R.drawable.ic_wifi,
+)
